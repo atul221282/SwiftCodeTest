2a3df1f939f82fb38900478756769ea4c443059e
Atul Chaudhary <atul221282@gmail.com>
2016-01-23 23:40:44 +1030

    Added empty test project

---
+++ SwiftDemo.Web.Test/ClientsControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SwiftDemo.Web.Test
+{
+    [TestClass]
+    public class ClientsControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
