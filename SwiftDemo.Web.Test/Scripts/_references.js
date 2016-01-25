8307c0d20c985b90f261ccf939dc303566a0671d
Atul Chaudhary <atul221282@gmail.com>
2016-01-25 15:03:30 +1030

    fixed the test

--- SwiftDemo.Web.Test/Scripts/_references.js
+++ SwiftDemo.Web.Test/Scripts/_references.js
@@ -1,17 +1,17 @@
-﻿
-
-
-/// <reference path="jasmine/jasmine.js" />
+﻿/// <reference path="jasmine/jasmine.js" />
 /// <reference path="sinon-1.9.1.js" />
 /// <reference path="../../swiftdemo.web/scripts/jquery-1.10.2.js" />
 /// <reference path="../../swiftdemo.web/scripts/bootstrap.min.js" />
 
 /// <reference path="../../swiftdemo.web/scripts/angular.min.js" />
 /// <reference path="../../swiftdemo.web/scripts/angular-block-ui.min.js" />
+/// <reference path="../../swiftdemo.web/scripts/angular-animate.js" />
 
 /// <reference path="../../swiftdemo.web/scripts/angular-mocks.js" />
 /// <reference path="../../swiftdemo.web/scripts/angular-ui-router.min.js" />
-/// <reference path="../../swiftdemo.web/scripts/angular-ui-notification.min.js" />
+/// <reference path="../../swiftdemo.web/scripts/toaster.js" />
+
+
 
 /// <reference path="../../swiftdemo.web/scripts/app/common/commonmodule.js" />
 /// <reference path="../../swiftdemo.web/scripts/app/mainmodule.js" />
