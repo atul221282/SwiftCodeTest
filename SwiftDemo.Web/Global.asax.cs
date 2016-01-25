9617cde993bbf3796e27f934136fbb9655a4b472
Atul Chaudhary <atul221282@gmail.com>
2016-01-25 22:18:08 +1030

    Created new branch swift code test

--- SwiftDemo.Web/Global.asax.cs
+++ SwiftDemo.Web/Global.asax.cs
@@ -19,6 +19,11 @@ namespace SwiftTest.Web
     /// <seealso cref="System.Web.HttpApplication" />
     public class Global : HttpApplication
     {
+        /// <summary>
+        /// Handles the Start event of the Application control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         void Application_Start(object sender, EventArgs e)
         {
             AreaRegistration.RegisterAllAreas();
